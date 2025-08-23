461018bffb63ace7a54ce75fca48e36fbd915a2c
Nurul Huda <nhsabz.learn@gmail.com>
2025-08-23 05:44:28 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,8 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
